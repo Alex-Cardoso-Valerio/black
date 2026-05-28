58006587a93a047182bac9e49e5541b5c3a84862
alexandre cardoso valerio <alecar12gamer@gmail.com>
2026-05-28 11:49:21 -0300

    add menu hamburger

--- main.js
+++ main.js
@@ -24,3 +24,23 @@
       if (t) { e.preventDefault(); t.scrollIntoView({ behavior: 'smooth' }); }
     });
   });
+// --- MENU HAMBÚRGUER ---
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+  const navItems = document.querySelectorAll('.nav-links a');
+
+  // Abrir e fechar o menu ao clicar no botão
+  if (hamburger) {
+    hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('active');
+      navLinks.classList.toggle('active');
+    });
+  }
+
+  // Fechar o menu automaticamente ao clicar num link
+  navItems.forEach(item => {
+    item.addEventListener('click', () => {
+      hamburger.classList.remove('active');
+      navLinks.classList.remove('active');
+    });
+  });
